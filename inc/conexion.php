ebc597f4c6ecbea175ec11ebcc74d7488a5ec9ea
Nicolas Alderete <nalderete@interbanking.com.ar>
2020-11-20 18:25:13 -0300

    agrego los valores de prod

--- inc/conexion.php
+++ inc/conexion.php
@@ -4,6 +4,10 @@
     $contrasena = "";
     $servidor = "localhost";
     $basededatos = "dietetica_db";
+    //$usuario = "id15397661_dietetica";
+    //$contrasena = "uQ]==O8]d(gXaGCJ";
+    //$servidor = "localhost";
+    //$basededatos = "id15397661_dietetica_db";
 
     $conexion = mysqli_connect($servidor, $usuario, $contrasena) or header("location: error.html");
 
