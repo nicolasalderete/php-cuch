042c239f0b889e45a89543d2e8b5c117622fc62b
OctavioMinteguia <72674657+OctavioMinteguia@users.noreply.github.com>
2021-06-16 00:45:37 -0300

    Update ofer_procesar.php

--- ofer_procesar.php
+++ ofer_procesar.php
@@ -1,32 +1,23 @@
 <?php
     include('inc/conexion.php');
 
+    $oferta=[];
+
     if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         if(empty($_POST["nombre"])){
             $nombre_error = "Por favor indique un nombre del producto";
         } else{
             $nombre = filter_var($_POST["nombre"], FILTER_SANITIZE_STRING);
+            $oferta[] = $nombre;
         }
 
         if(empty($_POST["descripcion"])){
             $descripcion_error = "Por favor indique un nombre del producto";
         } else{
             $descripcion = filter_var($_POST["descripcion"], FILTER_SANITIZE_STRING);
+            $oferta[] = $descripcion;
         }
-        
-        if(empty($_POST["categoria"])){
-            $categoria_error = "Por favor indique la categoria para el producto";
-        } else{
-            $categoria = filter_var($_POST["categoria"], FILTER_VALIDATE_INT);
-        }
-
-        /* Esto es lo que estaba, solo le puse una validacion si ingresaba una letra
-        en vez de un numero.
-        if(empty($_POST["precio"])){
-            $precio_error = "Por favor indique el precio para el producto";
-        } 
-        */
 
         if(is_numeric($_POST["precio"]) == false || empty($_POST["precio"]))
         {
@@ -35,6 +26,22 @@
          else
         {
             $precio = filter_var($_POST["precio"], FILTER_SANITIZE_STRING);
+            $oferta[] = $precio;
+        }
+
+        if(empty($_POST["prod_seleccionados"])){
+           
+            $prod_seleccionados_error = "Por favor seleccione un producto";
+        
+        } else{
+            $id = filter_var($_POST["id"], FILTER_SANITIZE_STRING);
+            $oferta[] = $id;
+
+            $_SESSION['prod_oferta'] = $oferta; 
+            
+            foreach ($i as $oferta => $id) {
+                echo "id: ".$id[$i];
+            }
         }
     }
 ?>
